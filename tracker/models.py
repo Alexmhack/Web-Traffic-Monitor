b781ffd779f8c467b9a8ca9540ba5185702b2d8b
alexHooper <alexmohack@gmail.com>
2018-11-23 20:32:38 +0530

    create model for page and session

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Page(models.Model):
+	name = models.CharField(max_length=256)
+	session = models.CharField(max_length=256)
+	first_visit = models.DateTimeField(auto_now=True)
+	visits = models.PositiveIntegerField()
+
+
+class Session(models.Model):
+	ip = models.GenericIPAddressField()
+	continent = models.CharField(max_length=20)
+	country = models.CharField(max_length=256)
+	city = models.CharField(max_length=256)
+	os = models.CharField(max_length=256)
+	browser = models.CharField(max_length=256)
+	session = models.CharField(max_length=256)
+	timestamp = models.DateTimeField(auto_now=True)
