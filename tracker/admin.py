30dbc6dbd288425d8ef9171f74f52673ed9bd651
alexHooper <alexmohack@gmail.com>
2018-11-23 20:44:30 +0530

    register models to admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Page, Session
+
+@admin.register(Page)
+class PageModelAdmin(admin.ModelAdmin):
+	list_display = ('name', 'session', 'first_visit', 'visits')
+	list_display_links = ('session', 'visits')
+	list_editable = ('name',)
+	search_fields = ('name', 'visits')
+	list_filter = ('name',)
+
+
+@admin.register(Session)
+class SessionModelAdmin(admin.ModelAdmin):
+	list_display = ('country', 'city', 'browser')
+	search_fields = ('country', 'city', 'browser', 'os')
+	list_filter = ('country', 'browser', 'os')
