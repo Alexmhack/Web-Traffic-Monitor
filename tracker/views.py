b2e95fa505b502eb42f5ded76cfa14cee0f8a49d
alexHooper <alexmohack@gmail.com>
2018-11-23 20:57:31 +0530

    instantiate pusher

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+import urllib.request
+import httpagentparser
+import os
+import hashlib
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import JsonResponse
+
+from pusher import Pusher
+from decouple import config
+
+pusher = Pusher(
+	app_id=config('PUSHER_APP_ID'),
+	key=config('PUSHER_KEY'),
+	secret=config('PUSHER_SECRET'),
+	cluster=config('PUSHER_CLUSTER'),
+	ssl=True
+)
